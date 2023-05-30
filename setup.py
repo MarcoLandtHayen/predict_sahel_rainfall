bfa83eb69eea1629e9690fda76bb5a201a92a217
MarcoLandtHayen <95050365+MarcoLandtHayen@users.noreply.github.com>
2023-05-30 11:10:27 +0200

    Update setup.py
    Add local scheme specification.

--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ setup(
         "write_to": "predict_sahel_rainfall/_version.py",
         "write_to_template": '__version__ = "{version}"',
         "tag_regex": r"^(?P<prefix>v)?(?P<version>[^\+]+)(?P<suffix>.*)?$",
-    }
+        "local_scheme": "node-and-date",
+    },
 )
