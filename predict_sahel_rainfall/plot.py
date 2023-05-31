c2be000ec5d835ec71d57679011444139b63f1f8
MarcoLandtHayen <95050365+MarcoLandtHayen@users.noreply.github.com>
2023-05-31 13:43:06 +0200

    Local precommit (#14)

--- predict_sahel_rainfall/plot.py
+++ predict_sahel_rainfall/plot.py
@@ -1,7 +1,8 @@
 import numpy as np
 
+
 # Define function to specify bar color, according to sign of values:
-def bar_color(data,color_pos,color_neg):
+def bar_color(data, color_pos, color_neg):
     """Create array of colors to be used e.g., in plots.
 
     Parameters
@@ -10,12 +11,12 @@ def bar_color(data,color_pos,color_neg):
         Containing numerical values.
     color_pos / color_neg: str
         Color codes to be assigned to positive and negative data values, respectively.
-        
+
     Returns
     -------
     np.array
         Array of strings containing color codes.
 
     """
-        
-    return np.where(data.values>0,color_pos,color_neg).T
+
+    return np.where(data.values > 0, color_pos, color_neg).T
--- tests/test_plot.py
+++ tests/test_plot.py
@@ -1,17 +1,19 @@
 # Tech preamble:
-import pytest
 import numpy as np
 import pandas as pd
+import pytest
 
 # Import functions to test:
 from predict_sahel_rainfall.plot import bar_color
 
+
 # Define test data:
-test_data = pd.DataFrame(data={'testdata': [1.0,-0.5,3.0,0.0]})
+test_data = pd.DataFrame(data={"testdata": [1.0, -0.5, 3.0, 0.0]})
+
 
 def test_bar_color():
     """Test, if function returns correct color codes according to values' signs."""
     assert all(
-        bar_color(data=test_data,color_pos='b',color_neg='r')[0]
-        == np.array(['b','r','b','r'])
+        bar_color(data=test_data, color_pos="b", color_neg="r")[0]
+        == np.array(["b", "r", "b", "r"])
     )
