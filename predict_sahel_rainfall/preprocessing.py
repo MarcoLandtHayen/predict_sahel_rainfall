9e8766df8bf6a99e5d84b0b6ff15fc48b863e59f
MarcoLandtHayen <malaha@gmx.net>
2023-06-02 20:48:49 +0200

    Add tests for preprocessing pipeline

--- predict_sahel_rainfall/preprocessing.py
+++ predict_sahel_rainfall/preprocessing.py
@@ -3,12 +3,13 @@ import numpy as np
 import pandas as pd
 
 
-def load_data(data_url, ESM, target_index, input_features, add_months, norm_target, lead_time):
-    
+def load_data(
+    data_url, ESM, target_index, input_features, add_months, norm_target, lead_time
+):
     """
     Function to load CICMoD data as inputs and target index with specified lead time.
     Optionally add months as additional one-hot encoded input feature and normalize target index:
-    
+
     Parameters:
     ===========
     data_url: str
@@ -26,22 +27,24 @@ def load_data(data_url, ESM, target_index, input_features, add_months, norm_targ
     lead_time: int
         Set lead time in months for target index.
         Needs to be positive (> 0).
-    
+
     Returns:
     ========
     inputs, target:
         Numpy arrays containing inputs and target.
-    
-    """    
+
+    """
 
     # Load data:
     climind = pd.read_csv(data_url)
 
     # Format data:
-    climind = climind.set_index(["model", "year", "month", "index"]).unstack(level=-1)["value"]
+    climind = climind.set_index(["model", "year", "month", "index"]).unstack(level=-1)[
+        "value"
+    ]
 
     # Extract data for specified model, reset index and drop columns year and month:
-    climind_ESM = climind.loc[(ESM)].reset_index().drop(columns=['year','month'])
+    climind_ESM = climind.loc[(ESM)].reset_index().drop(columns=["year", "month"])
 
     # Extract target index and keep ALL indices as inputs:
     target = climind_ESM.loc[:, climind_ESM.columns == target_index]
@@ -49,19 +52,45 @@ def load_data(data_url, ESM, target_index, input_features, add_months, norm_targ
 
     # If desired, add months as additional one-hot encoded input features:
     if add_months:
-
-        inputs['Jan']= (climind.loc[(ESM)].reset_index().drop(columns=['year']).loc[:,'month']==1).values.astype(int)
-        inputs['Feb']= (climind.loc[(ESM)].reset_index().drop(columns=['year']).loc[:,'month']==2).values.astype(int)
-        inputs['Mar']= (climind.loc[(ESM)].reset_index().drop(columns=['year']).loc[:,'month']==3).values.astype(int)
-        inputs['Apr']= (climind.loc[(ESM)].reset_index().drop(columns=['year']).loc[:,'month']==4).values.astype(int)
-        inputs['May']= (climind.loc[(ESM)].reset_index().drop(columns=['year']).loc[:,'month']==5).values.astype(int)
-        inputs['Jun']= (climind.loc[(ESM)].reset_index().drop(columns=['year']).loc[:,'month']==6).values.astype(int)
-        inputs['Jul']= (climind.loc[(ESM)].reset_index().drop(columns=['year']).loc[:,'month']==7).values.astype(int)
-        inputs['Aug']= (climind.loc[(ESM)].reset_index().drop(columns=['year']).loc[:,'month']==8).values.astype(int)
-        inputs['Sep']= (climind.loc[(ESM)].reset_index().drop(columns=['year']).loc[:,'month']==9).values.astype(int)
-        inputs['Oct']= (climind.loc[(ESM)].reset_index().drop(columns=['year']).loc[:,'month']==10).values.astype(int)
-        inputs['Nov']= (climind.loc[(ESM)].reset_index().drop(columns=['year']).loc[:,'month']==11).values.astype(int)
-        inputs['Dec']= (climind.loc[(ESM)].reset_index().drop(columns=['year']).loc[:,'month']==12).values.astype(int)   
+        inputs["Jan"] = (
+            climind.loc[(ESM)].reset_index().drop(columns=["year"]).loc[:, "month"] == 1
+        ).values.astype(int)
+        inputs["Feb"] = (
+            climind.loc[(ESM)].reset_index().drop(columns=["year"]).loc[:, "month"] == 2
+        ).values.astype(int)
+        inputs["Mar"] = (
+            climind.loc[(ESM)].reset_index().drop(columns=["year"]).loc[:, "month"] == 3
+        ).values.astype(int)
+        inputs["Apr"] = (
+            climind.loc[(ESM)].reset_index().drop(columns=["year"]).loc[:, "month"] == 4
+        ).values.astype(int)
+        inputs["May"] = (
+            climind.loc[(ESM)].reset_index().drop(columns=["year"]).loc[:, "month"] == 5
+        ).values.astype(int)
+        inputs["Jun"] = (
+            climind.loc[(ESM)].reset_index().drop(columns=["year"]).loc[:, "month"] == 6
+        ).values.astype(int)
+        inputs["Jul"] = (
+            climind.loc[(ESM)].reset_index().drop(columns=["year"]).loc[:, "month"] == 7
+        ).values.astype(int)
+        inputs["Aug"] = (
+            climind.loc[(ESM)].reset_index().drop(columns=["year"]).loc[:, "month"] == 8
+        ).values.astype(int)
+        inputs["Sep"] = (
+            climind.loc[(ESM)].reset_index().drop(columns=["year"]).loc[:, "month"] == 9
+        ).values.astype(int)
+        inputs["Oct"] = (
+            climind.loc[(ESM)].reset_index().drop(columns=["year"]).loc[:, "month"]
+            == 10
+        ).values.astype(int)
+        inputs["Nov"] = (
+            climind.loc[(ESM)].reset_index().drop(columns=["year"]).loc[:, "month"]
+            == 11
+        ).values.astype(int)
+        inputs["Dec"] = (
+            climind.loc[(ESM)].reset_index().drop(columns=["year"]).loc[:, "month"]
+            == 12
+        ).values.astype(int)
 
     # If desired, normalize target series to have zero mean and unit variance:
     if norm_target:
@@ -76,27 +105,26 @@ def load_data(data_url, ESM, target_index, input_features, add_months, norm_targ
     # Convert inputs and targets to numpy arrays:
     target = np.array(target)
     inputs = np.array(inputs)
-    
+
     # Return inputs and target:
     return inputs, target
 
 
 def split_sequence(data, input_length):
-    
     """
     Function to split data into sequences with specified number of time steps as input length:
-    
+
     Parameters:
     ===========
     data: Numpy array or Pandas DataFrame containing the time series data to be split.
     input_length: Integer number, specifies the number of time steps as input length.
-    
+
     Returns:
     ========
     Numpy array with data split into sequences.
-    
-    """    
-    
+
+    """
+
     X = list()
     for i in range(len(data)):
         # Find the end of this pattern:
@@ -111,10 +139,9 @@ def split_sequence(data, input_length):
 
 
 def train_val_test_split(inputs_split, target_cut, train_test_split, train_val_split):
-    
     """
     Function to split inputs and target into training, validation and test sets.
-    
+
     Parameters:
     ===========
     inputs_split, target_cut:
@@ -123,15 +150,15 @@ def train_val_test_split(inputs_split, target_cut, train_test_split, train_val_s
         Specify amount of combined training and validation data relative to test data.
     train_val_split: float
         Specify relative amount of combined training and validation used for training.
-    
+
     Returns:
     ========
     train_input, val_input, test_input: Numpy arrays
         Training, validation and test inputs.
     train_target, val_target, test_target: Numpy arrays
         Training, validation and test targets.
-        
-    """    
+
+    """
 
     # Get number of combined training and validation samples:
     n_train_val = int(train_test_split * len(inputs_split))
@@ -150,16 +177,15 @@ def train_val_test_split(inputs_split, target_cut, train_test_split, train_val_s
     val_input = train_val_input[n_train:]
     train_target = train_val_target[:n_train]
     val_target = train_val_target[n_train:]
-    
+
     # Return inputs and targets:
     return train_input, train_target, val_input, val_target, test_input, test_target
 
 
 def scale_norm_inputs(scale_norm, train_input, val_input, test_input, input_features):
-    
     """
     Function to optionally scale or normalize inputs.
-    
+
     Parameters:
     ===========
     scale_norm: str
@@ -172,70 +198,102 @@ def scale_norm_inputs(scale_norm, train_input, val_input, test_input, input_feat
         Numpy arrays containing training, validation and test inputs.
     input_features: array of str
         Select input features.
-        
+
     Returns:
     ========
     train_input_scaled, val_input_scaled, test_input_scaled: Numpy arrays
         Optionally scaled or normalized (or unchanged) training, validation and test inputs.
     train_mean, train_std, train_min, train_max: arrays of float
         Statistics from training data.
-            
-    """    
+
+    """
 
     ## Get statistics from training data:
 
     # Get mean, std dev, min and max for all input features, except optionally added one-hot encoded months
     # from training data:
-    train_mean = np.mean(train_input[:,:,:len(input_features)], axis=(0,1))
-    train_std = np.std(train_input[:,:,:len(input_features)], axis=(0,1))
-    train_min = np.min(train_input[:,:,:len(input_features)], axis=(0,1))
-    train_max = np.max(train_input[:,:,:len(input_features)], axis=(0,1))
+    train_mean = np.mean(train_input[:, :, : len(input_features)], axis=(0, 1))
+    train_std = np.std(train_input[:, :, : len(input_features)], axis=(0, 1))
+    train_min = np.min(train_input[:, :, : len(input_features)], axis=(0, 1))
+    train_max = np.max(train_input[:, :, : len(input_features)], axis=(0, 1))
 
     ## Optionally scale or normalize input features as specified:
 
     # 'scale_01': Scale input features with min/max scaling to [0,1].
     if scale_norm == "scale_01":
-
         # Copy unscaled inputs as initialization for scaled inputs:
         train_input_scaled = np.copy(train_input)
         val_input_scaled = np.copy(val_input)
         test_input_scaled = np.copy(test_input)
 
         # Apply scaling to all features, except optionally added one-hot encoded months:
-        train_input_scaled[:,:,:len(input_features)] = (train_input_scaled[:,:,:len(input_features)] - train_min) / (train_max - train_min)
-        val_input_scaled[:,:,:len(input_features)] = (val_input_scaled[:,:,:len(input_features)] - train_min) / (train_max - train_min)
-        test_input_scaled[:,:,:len(input_features)] = (test_input_scaled[:,:,:len(input_features)] - train_min) / (train_max - train_min)
+        train_input_scaled[:, :, : len(input_features)] = (
+            train_input_scaled[:, :, : len(input_features)] - train_min
+        ) / (train_max - train_min)
+        val_input_scaled[:, :, : len(input_features)] = (
+            val_input_scaled[:, :, : len(input_features)] - train_min
+        ) / (train_max - train_min)
+        test_input_scaled[:, :, : len(input_features)] = (
+            test_input_scaled[:, :, : len(input_features)] - train_min
+        ) / (train_max - train_min)
 
     # 'scale_11': Scale input features with min/max scaling to [-1,1].
     if scale_norm == "scale_11":
-
         # Copy unscaled inputs as initialization for scaled inputs:
         train_input_scaled = np.copy(train_input)
         val_input_scaled = np.copy(val_input)
         test_input_scaled = np.copy(test_input)
 
         # Apply scaling to all features, except optionally added one-hot encoded months:
-        train_input_scaled[:,:,:len(input_features)] = 2 * (train_input_scaled[:,:,:len(input_features)] - train_min) / (train_max - train_min) - 1
-        val_input_scaled[:,:,:len(input_features)] = 2 * (val_input_scaled[:,:,:len(input_features)] - train_min) / (train_max - train_min) - 1 
-        test_input_scaled[:,:,:len(input_features)] = 2 * (test_input_scaled[:,:,:len(input_features)] - train_min) / (train_max - train_min) - 1 
+        train_input_scaled[:, :, : len(input_features)] = (
+            2
+            * (train_input_scaled[:, :, : len(input_features)] - train_min)
+            / (train_max - train_min)
+            - 1
+        )
+        val_input_scaled[:, :, : len(input_features)] = (
+            2
+            * (val_input_scaled[:, :, : len(input_features)] - train_min)
+            / (train_max - train_min)
+            - 1
+        )
+        test_input_scaled[:, :, : len(input_features)] = (
+            2
+            * (test_input_scaled[:, :, : len(input_features)] - train_min)
+            / (train_max - train_min)
+            - 1
+        )
 
     # 'norm': Normalize input features, hence subtract mean and divide by std dev.
     if scale_norm == "norm":
-
         # Copy unscaled inputs as initialization for scaled inputs:
         train_input_scaled = np.copy(train_input)
         val_input_scaled = np.copy(val_input)
         test_input_scaled = np.copy(test_input)
 
         # Normalize all features, except optionally added one-hot encoded months:
-        train_input_scaled[:,:,:len(input_features)] = (train_input_scaled[:,:,:len(input_features)] - train_mean) / train_std
-        val_input_scaled[:,:,:len(input_features)] = (val_input_scaled[:,:,:len(input_features)] - train_mean) / train_std
-        test_input_scaled[:,:,:len(input_features)] = (test_input_scaled[:,:,:len(input_features)] - train_mean) / train_std
+        train_input_scaled[:, :, : len(input_features)] = (
+            train_input_scaled[:, :, : len(input_features)] - train_mean
+        ) / train_std
+        val_input_scaled[:, :, : len(input_features)] = (
+            val_input_scaled[:, :, : len(input_features)] - train_mean
+        ) / train_std
+        test_input_scaled[:, :, : len(input_features)] = (
+            test_input_scaled[:, :, : len(input_features)] - train_mean
+        ) / train_std
 
     # else: Keep raw input features.
-    
+
     # Return scaled inputs and statistics from training data:
-    return train_input_scaled, val_input_scaled, test_input_scaled, train_mean, train_std, train_min, train_max
+    return (
+        train_input_scaled,
+        val_input_scaled,
+        test_input_scaled,
+        train_mean,
+        train_std,
+        train_min,
+        train_max,
+    )
 
 
 def prepare_inputs_and_target(
@@ -251,7 +309,6 @@ def prepare_inputs_and_target(
     train_val_split,
     scale_norm,
 ):
-    
     """
     Function to wrap up the complete preprocessing pipeline:
     Load CICMoD data as inputs and target index with specified lead time.
@@ -259,7 +316,7 @@ def prepare_inputs_and_target(
     Split data into sequences with specified number of time steps.
     Split inputs and target into training, validation and test sets.
     Optionally scale or normalize inputs.
-    
+
     Parameters:
     ===========
     data_url: str
@@ -289,7 +346,7 @@ def prepare_inputs_and_target(
         'scale_01': Scale input features with min/max scaling to [0,1].
         'scale_11': Scale input features with min/max scaling to [-1,1].
         'norm': Normalize input features, hence subtract mean and divide by std dev.
-    
+
     Returns:
     ========
     train_input, val_input, test_input: Numpy arrays
@@ -298,25 +355,25 @@ def prepare_inputs_and_target(
         Corresponding targets, optionally normalized.
     train_mean, train_std, train_min, train_max: arrays of float
         Statistics from training data.
-    
-    """    
+
+    """
 
     # Load data:
     inputs, target = load_data(
         data_url=data_url,
         ESM=ESM,
         target_index=target_index,
-        input_features=input_features, 
-        add_months=add_months, 
-        norm_target=norm_target, 
+        input_features=input_features,
+        add_months=add_months,
+        norm_target=norm_target,
         lead_time=lead_time,
     )
-    
+
     # Split inputs into sequences of specified input length:
     inputs_split = split_sequence(data=inputs, input_length=input_length)
 
     # Adjust target: Cut first (input_length - 1) entries
-    target_cut = target[input_length-1:]
+    target_cut = target[input_length - 1 :]
 
     # Split inputs and targets into training, validation and test sets:
     (
@@ -325,12 +382,13 @@ def prepare_inputs_and_target(
         val_input,
         val_target,
         test_input,
-        test_target
-    ) = train_val_test_split(inputs_split=inputs_split,
-                             target_cut=target_cut,
-                             train_test_split=0.9,
-                             train_val_split=0.8,
-                            )
+        test_target,
+    ) = train_val_test_split(
+        inputs_split=inputs_split,
+        target_cut=target_cut,
+        train_test_split=0.9,
+        train_val_split=0.8,
+    )
 
     # Optionally scale or normalize inputs:
     (
---
+++ tests/test_preprocessing.py
@@ -0,0 +1,164 @@
+# Tech preamble:
+import numpy as np
+import pandas as pd
+import pytest
+from numpy.testing import assert_almost_equal
+
+# Import functions to test:
+from predict_sahel_rainfall.preprocessing import (
+    load_data,
+    split_sequence,
+    train_val_test_split,
+    scale_norm_inputs,
+    prepare_inputs_and_target,
+)
+
+# Create dummy array:
+dummy_array = np.array(
+    [[1.0,11.0],
+     [2.0,12.0],
+     [3.0,13.0],
+     [4.0,14.0],
+     [5.0,15.0],
+     [6.0,16.0],
+     [7.0,17.0],
+     [8.0,18.0],
+     [9.0,19.0],
+     [10.0,20.0]]
+)
+
+# Set parameters for test:
+data_url="https://github.com/MarcoLandtHayen/climate_index_collection/releases/download/v2023.03.29.1/climate_indices.csv"
+target_index='PREC_SAHEL'
+input_features=['PREC_SAHEL', 'SAM_ZM']
+add_months=True
+norm_target=True
+lead_time=1
+input_length=3
+train_test_split = 0.9
+train_val_split = 0.8
+
+
+@pytest.mark.parametrize("ESM", list(['CESM','FOCI']))
+def test_load_data(ESM):
+    """Test, if function correctly loads test csv."""
+    
+    # Load data:
+    inputs, target = load_data(
+        data_url=data_url,
+        ESM=ESM,
+        target_index=target_index,
+        input_features=input_features, 
+        add_months=add_months, 
+        norm_target=norm_target, 
+        lead_time=lead_time,
+    )
+    
+    # Test, if months are addes as additional one-hot encoded input features:
+    assert inputs.shape[1] == len(input_features)+12
+    
+    # Test, if we end up with only single target feature
+    assert target.shape[1] == 1
+    
+
+def test_split_sequence():
+    """Test, if function correctly splits data into sequences of specified input length."""
+    
+    # Split dummy array into sequences of specified input length:
+    inputs_split = split_sequence(data=dummy_array, input_length=input_length)
+    
+    # Test, if number of resulting sequences is correct:
+    assert inputs_split.shape[0] == len(dummy_array)-(input_length-1)
+    
+
+def test_train_val_test_split():
+    """Test, if function correctly splits inputs and target into training, validation and test sets."""
+    
+    # Split dummy array into sequences of specified input length:
+    dummy_inputs_split = split_sequence(data=dummy_array, input_length=input_length)
+    
+    # Create corresponding dummy target:
+    dummy_target_cut = dummy_array[input_length-1:,0]
+    
+    # Split dummy inputs and target into training, validation and test sets:
+    (
+        train_input,
+        train_target,
+        val_input,
+        val_target,
+        test_input,
+        test_target
+    ) = train_val_test_split(inputs_split=dummy_inputs_split,
+                             target_cut=dummy_target_cut,
+                             train_test_split=train_test_split,
+                             train_val_split=train_val_split,
+                            )
+    
+    # Test dimensions of training, validation and test inputs and targets:
+    assert train_input.shape == (5,3,2)
+    assert val_input.shape == (2,3,2)
+    assert test_input.shape == (1,3,2)
+    assert train_target.shape == (5,)
+    assert val_target.shape == (2,)
+    assert test_target.shape == (1,)
+
+    
+@pytest.mark.parametrize("scale_norm", list(['scale_01','scale_11','norm']))
+def test_scale_norm_inputs(scale_norm):
+    """Test, if training inputs are correctly normalized or scaled."""
+    
+    # Split dummy array into sequences of specified input length:
+    dummy_inputs_split = split_sequence(data=dummy_array, input_length=input_length)
+    
+    # Create corresponding dummy target:
+    dummy_target_cut = dummy_array[input_length-1:]
+    
+    # Split dummy inputs and target into training, validation and test sets:
+    (
+        train_input,
+        train_target,
+        val_input,
+        val_target,
+        test_input,
+        test_target
+    ) = train_val_test_split(inputs_split=dummy_inputs_split,
+                             target_cut=dummy_target_cut,
+                             train_test_split=train_test_split,
+                             train_val_split=train_val_split,
+                            )
+    # Scale or normalize inputs:
+    (
+        train_input,
+        val_input,
+        test_input,
+        train_mean,
+        train_std,
+        train_min,
+        train_max,
+    ) = scale_norm_inputs(
+        scale_norm=scale_norm,
+        train_input=train_input,
+        val_input=val_input,
+        test_input=test_input,
+        input_features=input_features,
+    )
+    
+    ## Test scaling or normalization of training inputs:
+    
+    # Scale inputs to [0,1]:
+    if scale_norm=='scale_01':
+        assert np.min(train_input) == 0
+        assert np.max(train_input) == 1
+        
+    # Scale inputs to [-1,1]:
+    if scale_norm=='scale_11':
+        assert np.min(train_input) == -1
+        assert np.max(train_input) == 1
+        
+    # Normalize inputs to have zero mean and unit variance:
+    if scale_norm=='norm':
+        assert_almost_equal(actual=np.mean(train_input), desired=0, decimal=3)
+        assert_almost_equal(actual=np.std(train_input), desired=1, decimal=3)
+    
+    
+        
