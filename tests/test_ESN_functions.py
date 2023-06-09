c97b05134f4bd9ed3d768c5dab43f57c1fd1051a
MarcoLandtHayen <malaha@gmx.net>
2023-06-09 15:45:17 +0200

    Modify test for ESN function to set up ESN model

--- tests/test_ESN_functions.py
+++ tests/test_ESN_functions.py
@@ -9,7 +9,7 @@ from tensorflow.keras.layers import Dense, Input, Lambda, concatenate
 from tensorflow.keras.models import Model
 
 # Import functions to test:
-from predict_sahel_rainfall.ESN_functions import ESN, predESN, setESN, trainESN
+from predict_sahel_rainfall.ESN_functions import ESN, setESN
 
 
 # Set parameters for test:
